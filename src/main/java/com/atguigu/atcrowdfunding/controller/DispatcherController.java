e1fed32e9b31cf16ec2e0e7d0e49ba0a67baadd5
wuwenshuai <wuwenshuai@lattebank.com>
2018-09-17 17:31:46 +0800

    第三天

--- src/main/java/com/atguigu/atcrowdfunding/controller/DispatcherController.java
+++ src/main/java/com/atguigu/atcrowdfunding/controller/DispatcherController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import com.atguigu.atcrowdfunding.util.AjaxResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.atguigu.atcrowdfunding.bean.User;
 import com.atguigu.atcrowdfunding.manager.service.UserService;
 import com.atguigu.atcrowdfunding.util.Const;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DispatcherController {
@@ -36,17 +38,36 @@ public class DispatcherController {
 	}
 	
 	@RequestMapping("/doLogin")
-	public String doLogin(String loginacct,String userpswd,String type,HttpSession session){
+	@ResponseBody
+	public AjaxResult doLogin(String loginacct, String userpswd, String type, HttpSession session){
 		Map<String,Object> paramMap = new HashMap<String,Object>();
 		paramMap.put("loginacct", loginacct);
 		paramMap.put("userpswd", userpswd);
-		paramMap.put("type", type);		
-		
-		User user = userService.queryUserlogin(paramMap);
-		
-		session.setAttribute(Const.LOGIN_USER, user);
-		
-		return "redirect:/main.htm";
+		paramMap.put("type", type);
+		AjaxResult result = new AjaxResult();
+
+		try {
+			User user = userService.queryUserlogin(paramMap);
+			session.setAttribute(Const.LOGIN_USER, user);
+			result.setSuccess(true);
+			result.setMessage("success");
+		}catch (Exception e){
+			e.printStackTrace();
+			result.setSuccess(false);
+			result.setMessage("用户名或者密码错误");
+		}
+		return result;
+
+	}
+
+
+	//退出系统
+	@RequestMapping("/logout")
+	public String logout(HttpSession session){
+
+		//清除session
+		session.invalidate();
+		return "redirect:index.htm";
 	}
 	
 }
